d5bfd78f60c8209d7e5e3140ee94592e7d4fbe98
sdongpil <pildong94@naver.com>
2024-01-03 19:19:23 +0900

    템플릿 메서드 적용

--- src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
+++ src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -35,4 +35,25 @@ public class TemplateMethodTest {
         AbstractTemplate template2 = new SubClassLogic2();
         template2.execute();
     }
+
+    @Test
+    void templateMethod2() {
+        AbstractTemplate template1 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직 1 실행");
+            }
+        };
+        log.info("클래스 이름1 ={}",template1.getClass());
+        template1.execute();
+
+        AbstractTemplate template2 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직 2 실행");
+            }
+        };
+        log.info("클래스 이름1 ={}",template2.getClass());
+        template2.execute();
+    }
 }
